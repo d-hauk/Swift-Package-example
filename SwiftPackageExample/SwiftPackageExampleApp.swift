771b8a8c73f3a0e4bd7b1d55d81c0f05c5e6965d
Dana <dana@larky.com>
2023-03-29 14:21:29 -0400

    Initial Commit

---
+++ SwiftPackageExample/SwiftPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftPackageExampleApp.swift
+//  SwiftPackageExample
+//
+//  Created by Dana Haukoos on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
